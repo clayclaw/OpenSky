fb59d9f787f34f044ecdb9591c353fe7dfd77f29
clayclaw <10925519+clayclaw@users.noreply.github.com>
2024-04-06 01:18:48 +0800

    add system config

--- plugin/src/main/kotlin/io/github/clayclaw/opensky/config/BaseConfig.kt
+++ plugin/src/main/kotlin/io/github/clayclaw/opensky/config/BaseConfig.kt
@@ -2,9 +2,11 @@ package io.github.clayclaw.opensky.config
 
 import io.github.clayclaw.opensky.config.comp.ConfigCache
 import io.github.clayclaw.opensky.config.comp.ConfigDatabase
+import io.github.clayclaw.opensky.config.comp.ConfigSystem
 
 data class BaseConfig(
     var serverId: String = "opensky-server",
     var database: ConfigDatabase = ConfigDatabase(),
     var cache: ConfigCache = ConfigCache(),
+    var system: ConfigSystem = ConfigSystem(),
 ) {}
---
+++ plugin/src/main/kotlin/io/github/clayclaw/opensky/config/comp/ConfigSystem.kt
@@ -0,0 +1,5 @@
+package io.github.clayclaw.opensky.config.comp
+
+data class ConfigSystem(
+    var serverKeepaliveTimeMillis: Long = 60000,
+)
